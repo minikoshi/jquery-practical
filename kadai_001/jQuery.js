6ca93f008df56c2972f5501abbb0c3a6d11ccc6a
minikoshi <y-l-c@i.softbank.jp>
2025-02-12 23:37:15 +0900

    kadai_001を修正

--- kadai_001/jQuery.js
+++ kadai_001/jQuery.js
@@ -31,12 +31,18 @@ $(function() {
     }
   });
   // スムーズスクロール
-  $('a[href^="#"]').click(function(){
-    var speed = 500;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
+  $('a[href^="#"]').click(function() {
+    const speed = 500;
+    const href= $(this).attr("href");
+    let $target;
+    if(href == '#') {
+      $target = $('html');
+    }
+    else {
+      $target = $(href);
+    }
+    const position = $target.offset().top;
+    $("html, body").animate({'scrollTop': position }, speed, "swing");
     return false;
   });
   // スクロールしてフェードイン
